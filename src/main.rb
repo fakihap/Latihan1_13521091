ab4d39d7c969bdcb55c6ede42696a4471e8a270b
fakihap <fakih.anugerah@gmail.com>
2023-03-29 19:09:20 +0700

    feat : matrix update

--- src/main.rb
+++ src/main.rb
@@ -5,3 +5,7 @@ puts "This is main Ruby Program"
 
 kei = Matrix.new(2, 9)
 kei.describe()
+
+zai = [[], []]
+kei.setMatrix(zai)
+kei.describe()
--- src/matrix.rb
+++ src/matrix.rb
@@ -3,6 +3,17 @@ class Matrix
         @width, @height = w, h
     end
 
+    def setMatrix(array)
+        if array.length > 0 and array[0].length > 0
+            @height = array.length
+            @width = array[0].length
+
+            @array = array
+        else
+            puts "WARNING : failed to setMatrix"
+        end
+    end
+
     def describe()
         puts "width is #{@width}", 
              "height is #{@height}"
