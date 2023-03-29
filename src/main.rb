5ceefe53c820158a3d9eb9c9640eca372bcfa7e3
fakihap <fakih.anugerah@gmail.com>
2023-03-30 02:49:12 +0700

    chore : main IO

--- src/main.rb
+++ src/main.rb
@@ -1,35 +1,51 @@
 require_relative "matrix.rb"
 
-
-puts "\t   Program Penyelesaian TSP", "\tmenggunakan Branch and Bound", "\t     dan Reduced Matrix", "\n"
-
-kei = Matrix.new
-
-
-# zai = [[0, 3, 9], [-1, 0, 1], [-1, -1, -1]]
-# zai = [
-#    [12, 20, 30, 10, 11],
-#    [15, 8, 16, 4, 2],
-#    [3, 5, 11, 2, 4],
-#    [19, 6, 18, 9, 3],
-#    [16, 4, 7, 16, 8]
-# ]
-# zai = [
+# lat1 = [
 #    [-1, 20, 30, 10, 11],
 #    [15, -1, 16, 4, 2],
 #    [3, 5, -1, 2, 4],
 #    [19, 6, 18, -1, 3],
 #    [16, 4, 7, 16, -1]
 # ]
-zai = [
-   [-1, 2, 7, 8],
-   [6, -1, 3, 7],
-   [5, 8, -1, 4],
-   [7, 6, 9, -1]
-]
-kei.setMatrix(zai)
-# kei.describe()
 
-# puts kei.isReduced(zai)
+# lat2 = [
+#    [-1, 2, 7, 8],
+#    [6, -1, 3, 7],
+#    [5, 8, -1, 4],
+#    [7, 6, 9, -1]
+# ]
 
-kei.solve(3)
+costMat = Matrix.new
+
+while true
+   puts  "\t   Program Penyelesaian TSP", 
+      "\tmenggunakan Branch and Bound", 
+      "\t     dan Reduced Matrix", 
+      "\n"
+
+   puts "Masukkan banyak simpul yang dimiliki : "
+   simpulCount = gets.to_i
+
+   arr = Array.new(simpulCount) { Array.new(simpulCount) }
+   puts ""
+
+   puts "Masukkan matrix ongkos : "
+   puts "NOTE : masukkan Infinite sebagai -1"
+   for i in (0...simpulCount)
+      inp = gets.strip.split(" ")
+
+      for j in (0...simpulCount)
+         arr[i][j] = inp[j].to_i
+      end
+   end
+   costMat.setMatrix(arr)
+   puts ""
+
+   puts "Masukkan simpul awal solusi : "
+   start = gets.to_i
+   puts ""
+
+   costMat.solve(start)
+
+   puts "\n\n"
+end
